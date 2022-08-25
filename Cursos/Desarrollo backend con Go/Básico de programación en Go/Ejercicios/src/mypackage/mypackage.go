a1309a49ebb24a086234a7315e5e7ef91927207c
Gerardo developer <gerardo.software.developer@gmail.com>
2022-08-25 20:36:19 -0300

    Se reorganizo las carpetas

--- Cursos/Desarrollo backend con Go/Básico de programación en Go/Ejercicios/src/mypackage/mypackage.go
+++
@@ -1,7 +0,0 @@
-package mypackage //1ro va el nombre del paquete
-
-//CarPublic Car con acceso público
-type CarPublic struct {
-	Brand string
-	Year  int
-}
--- Cursos/Desarrollo backend con Python y Django/1- Básico/3- Curso de Python/Ejercicios/Modulos/main.py
+++
@@ -1,9 +0,0 @@
-from lamp import Lamp
-
-
-
-
-if __name__ == '__main__':
-    lamp = Lamp()
-    lamp.turn_on()#Prende la lampara
-    lamp.turn_off()#Apaga la lampara
