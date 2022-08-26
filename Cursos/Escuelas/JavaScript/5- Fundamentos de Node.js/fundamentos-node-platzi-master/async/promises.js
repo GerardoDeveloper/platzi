72260d449d104ff6fed5362a2c619fd9c2bf30af
Gerardo developer <gerardo.software.developer@gmail.com>
2022-08-25 21:06:28 -0300

    Se eliminaron espacios

--- Cursos/Escuelas/JavaScript/5- Fundamentos de Node.js/fundamentos-node-platzi-master/async/promises.js
+++ Cursos/Escuelas/JavaScript/5- Fundamentos de Node.js/fundamentos-node-platzi-master/async/promises.js
@@ -1,16 +1,15 @@
 function hola(nombre) {
     return new Promise(function (resolve, reject) {
         setTimeout(function () {
-            console.log('Hola, '+ nombre);
+            console.log('Hola, ' + nombre);
             resolve(nombre);
         }, 1500);
     });
-    
 }
 
 function hablar(nombre) {
-    return new Promise( (resolve, reject) => {
-        setTimeout(function() {
+    return new Promise((resolve, reject) => {
+        setTimeout(function () {
             console.log('Bla bla bla bla...');
             //resolve(nombre);
             reject('Hay un error');
@@ -19,15 +18,13 @@ function hablar(nombre) {
 }
 
 function adios(nombre) {
-    return new Promise( (resolve, reject) => {
-        setTimeout(function() {
+    return new Promise((resolve, reject) => {
+        setTimeout(function () {
             console.log('Adios', nombre);
             resolve();
         }, 1000);
     });
 }
-
-// ---
 
 console.log('Iniciando el proceso..');
 hola('Carlos')
